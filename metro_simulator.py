037a5334c7d47a60ea72bb9e798c74a0f3293bab
The Mridul <mriduljain208@gmail.com>
2025-11-23 06:43:45 +0530

    module metro timings✅

--- metro_simulator.py
+++ metro_simulator.py
@@ -1,19 +1,13 @@
-from datetime import datetime
-current_datetime = datetime.now()
+from datetime import datetime,timedelta
+now = datetime.now()
+timeNow=now.strftime("%H:%M")
 
-# Frequency:
-#   Off-peak hours: every 8 minutes
-#   Peak hours (8–10 AM and 5–7 PM): every 4 minutes
-
-def servicehours(start_hr=6, start_min=0, end_hr=23, end_min=0):
-    
-    now = datetime.now()
-    start_time = now.replace(hour=start_hr, minute=start_min, second=0, microsecond=0)
-    end_time = now.replace(hour=end_hr,minute=end_min)
-
-    if now > start_time and now < end_time :
-        return True
-    return False
+time6AM=now.replace(hour=6,minute=0,second=0,microsecond=0)
+time8AM=now.replace(hour=8,minute=0,microsecond=0,second=0)
+time10AM=now.replace(hour=10,minute=0,microsecond=0,second=0)
+time5PM=now.replace(hour=17,minute=0,microsecond=0,second=0)
+time7PM=now.replace(hour=19,minute=0,microsecond=0,second=0)
+time11PM=now.replace(hour=23,minute=0,microsecond=0,second=0)
 
 def read_section(filename, start_marker, end_marker):
     stations=[]
@@ -23,7 +17,7 @@ def read_section(filename, start_marker, end_marker):
         for line in file:
             if start_marker in line:
                 inSection = True
-
+            
             if end_marker in line:
                 inSection = False
                 break 
@@ -38,21 +32,92 @@ def read_section(filename, start_marker, end_marker):
 stationsBlue= read_section("metro_data.txt", "BLUE LINE STATIONS", "[END BLUE]")
 stationsMagenta= read_section("metro_data.txt", "MAGENTA LINE STATIONS", "[END MAGENTA]")
 
-sourceLine = 0
-count = 0
-while sourceLine != "Blue" and sourceLine != "Magenta": 
-    if count==0:
-        count=1
-        sourceLine = input("Enter the line ur on (Blue or Magenta): ")
-    elif count!=0:
-        sourceLine = input("Enter valid line (Blue or Magenta): ")
-if sourceLine == "Blue" :
-    for stations in stationsBlue:
-        print(stations)
-if sourceLine== "Magenta" :
-    for stations in stationsMagenta:
-        print(stations)
+def stationSelector():
+    valid_lines = ["blue", "magenta"]
+    while True:
+        line = input("Enter the line (Blue or Magenta): ").strip().lower()
+        if line in valid_lines:
+            break
+        print("Invalid line. Please enter 'Blue' or 'Magenta'.")
+    if line.lower() == "blue":
+        for station in stationsBlue:
+            print(station)
+    if line.lower() == "magenta":
+        for station in stationsMagenta:
+            print(station)
+    station = input("Enter Station using Ids:")
+    return station
 
-sourceStation= input("Enter the station ur on using ID: ")
+def metroTimings(now):
+    tempTimings = []
+    timings=[]
+    if time6AM<=now<time8AM:
+        time = time6AM
+        while time < time8AM:
+            tempTimings.append(time)
+            time += timedelta(minutes=8)
+        for time in tempTimings:
+            if time>=now:
+                timings.append(time)
+        finalTimings = [t.strftime("%H:%M") for t in timings]
+        return finalTimings
+    
+    elif time8AM<=now<time10AM:
+        time = time8AM
+        while time < time10AM:
+            tempTimings.append(time)
+            time += timedelta(minutes=4)
+        for time in tempTimings:
+            if time>=now:
+                timings.append(time)
+        finalTimings = [t.strftime("%H:%M") for t in timings]
+        return finalTimings
+    
+    elif time10AM<=now<time5PM:
+        if time10AM<=now<=time5PM:
+            time = time6AM
+        while time < time5PM:
+            tempTimings.append(time)
+            time += timedelta(minutes=8)
+        for time in tempTimings:
+            if time>=now:
+                timings.append(time)
+        finalTimings = [t.strftime("%H:%M") for t in timings]
+        return finalTimings
+    
+    elif time5PM<=now<time7PM:
+        if time5PM<=now<=time7PM:
+            time = time5PM
+        while time < time7PM:
+            tempTimings.append(time)
+            time += timedelta(minutes=4)
+        for time in tempTimings:
+            if time >= now:
+                timings.append(time)
+        finalTimings = [t.strftime("%H:%M") for t in timings]
+        return finalTimings
+    
+    elif time5PM<=now<time7PM:
+        if time5PM<=now<=time7PM:
+            time = time5PM
+        while time < time7PM:
+            tempTimings.append(time)
+            time += timedelta(minutes=4)
+        for time in tempTimings:
+            if time>=now:
+                timings.append(time)
+        finalTimings = [t.strftime("%H:%M") for t in timings]
+        return finalTimings
+    else:
+        return "No service available"
 
-# modeSelector= input("Enter Mode, \n 'A' for Metro Timings, \n 'B' for Trip Plannar: ")
+
+def modeSelector(now,sourceStation):
+    mode= input("Enter Mode, \n 'A' for Metro Timings, \n 'B' for Trip Plannar:")
+    if mode.lower()=="a":
+        return metroTimings(now)
+    elif mode.lower() == "b":
+        return "under development"
+
+sourceStation= stationSelector()
+print(modeSelector(now,sourceStation))
